e1ed7669a7326e61f086f745a7b010eab22118ec
DFSIT <baha.legendary@gmail.com>
2016-07-01 09:45:43 +0800

    fix redundancy

--- guba_eastmoney_crawler_py3.4.1.py
+++ guba_eastmoney_crawler_py3.4.1.py
@@ -72,14 +72,23 @@ def save_csv(path,data):
         for item in data:
             spamwriter.writerow(item)
 
+
+def save_csv_w(path,data):
+    with open(path,'w', newline='',encoding='utf-8') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter='|')
+        for item in data:
+            spamwriter.writerow(item)
+
+
 def get_post_vol(content):
     if len(content)==1:
         return 0
     else:
-        t1=datetime.datetime.strptime(content[1][0],'%Y-%m-%d %H:%M:%S')
+        t1=datetime.datetime.strptime(content[0][0],'%Y-%m-%d %H:%M:%S')
         t2=datetime.datetime.strptime(content[-1][0],'%Y-%m-%d %H:%M:%S')
         seconds=(t2-t1).seconds
-        post_count=len(content)
+        # print(seconds)
+        post_count=int(content[-1][1])-int(content[0][1])
         post_vol=post_count/seconds
     return post_vol
 
@@ -94,7 +103,7 @@ def read_csv(path):
 def get_max_id(path):
     content_list=read_csv(path)
     sort_content_by_idcode(content_list)
-    return content_list[-1][1],content_list[-1][0]
+    return int(content_list[-1][1]),datetime.datetime.strptime(content_list[-1][0][:10],'%Y-%m-%d').date()
 
 def get_start_id_date(path,start_id):
     url='http://guba.eastmoney.com/news,0,%s.html'%start_id
@@ -109,10 +118,11 @@ def get_start_id_date(path,start_id):
 
     return_datetime_date_html=datetime.datetime.strptime(post_time,'%Y-%m-%d').date()
 
-
-
     path_list=os.listdir(path)
+    path_list.sort(key=lambda x:datetime.datetime.strptime(x,'%Y-%m-%d'))
+    path_list.reverse()
     doc_count=len(path_list)
+
     if doc_count==0:
         return start_id,return_datetime_date_html
     else:
@@ -146,7 +156,7 @@ class Async_infi_Spider():
         self.save_content_today=[]
         self.save_content_tomorrow=[]
         self.homepage_id=idcode
-        self.concurrency_temp_step=concurrency
+        self.concurrency_temp_step=10
 
 
         self.post_vol=1
@@ -159,9 +169,13 @@ class Async_infi_Spider():
         self.date_today_datetime=datetime_date
         self.date_today_string=self.date_today_datetime.strftime('%Y-%m-%d')
         self.date_tomorrow_datetime=self.date_today_datetime+datetime.timedelta(days=1)
-        self.date_tomorrow_string=self.date_tomorrow_datetime.strftime('&Y-%m-%d')
+        self.date_tomorrow_string=self.date_tomorrow_datetime.strftime('%Y-%m-%d')
         self.csv_saving_path_today=self.path+self.date_today_string+'/'+self.date_today_string+'.csv'
-        self.log_saving_path_today=self.path+self.date_today_string+'/'+self.date_today_string+'_log.txt'
+        self.log_saving_path_today=self.path+self.date_today_string+'/'+self.date_today_string+'_log.csv'
+
+
+        self.sleep_flag=1
+
 
 
 
@@ -178,9 +192,9 @@ class Async_infi_Spider():
             else:
                 resp.close()
         except Exception as e:
-            with open(self.log_saving_path_today,'a', newline='',encoding='utf-8') as f:
-                f.write(idcode)
-                f.write(e)
+            with open(self.log_saving_path_today,'a', newline='',encoding='utf-8') as csvfile:
+                spamwriter=csv.writer(csvfile, delimiter='|')
+                spamwriter.writerow([idcode,e.__str__()])
             print(e)
 
 
@@ -208,14 +222,14 @@ class Async_infi_Spider():
             article_content=tag.find('div',class_="stockcodec").get_text()
             self.content.append([post_time,idcode,bar,title,author,article_content])
         except Exception as e:
-            print(idcode,e,'in handle')
+            print(str(idcode),e,'in handle')
 
 
     def _run(self):
         while 1:
             t1=time.time()
             for i in sep(self.input_start_code,self.concurrency,self.num_per):
-                print('sending %s requests'%i[1])
+                print('sending %s requests'%i[1],self.input_start_code,self.concurrency,self.num_per)
                 self.work(i[0],i[1])
 
             if not os.path.exists(self.path+self.date_today_string+'/'):
@@ -235,6 +249,7 @@ class Async_infi_Spider():
                 continue
 
             elif self.return_count!=0:
+
                 '''先排序 再删除异常值'''
                 print('获取 %s 条内容 整理中'%self.return_count)
                 sort_content_by_date(self.content)
@@ -249,7 +264,7 @@ class Async_infi_Spider():
                     self.post_vol=get_post_vol(self.content)
                     '''取最新爬帖的发表时间'''
                     self.last_post_datetime=datetime.datetime.strptime(self.content[-1][0],'%Y-%m-%d %H:%M:%S')
-                    print('获取发帖速度参数和最后发帖时间...',self.post_vol,self.last_post_datetime)
+                    print('获取发帖速度参数和最后发帖时间...','%.2f'%self.post_vol,self.last_post_datetime)
                     '''判断是否完成当天任务'''
                     if self.content[-1][0][:10]==self.date_tomorrow_string:
                         print('到达时间分隔')
@@ -257,20 +272,25 @@ class Async_infi_Spider():
                         self.save_content_today_tomorrow=sep_content_today_tomorrow(self.content,self.date_today_string)
                         self.save_content_today=self.save_content_today_tomorrow[0]
                         self.save_content_tomorrow=self.save_content_today_tomorrow[1]
+                        self.save_content_today_tomorrow=[]
+
                         self.content=self.save_content_tomorrow
+                        self.save_content_tomorrow=[]
                         save_csv(self.csv_saving_path_today,self.save_content_today)
+                        self.save_content_today=[]
                         print('数据存储完毕')
-                        '''数据整理'''
-                        temp_tank_list=read_csv(self.csv_saving_path_today)
-                        sort_content_by_date(temp_tank_list)
-                        save_csv(self.csv_saving_path_today,temp_tank_list)
-                        del temp_tank_list
-                        print('数据整理完毕')
+                        # '''数据整理'''
+                        #
+                        # temp_tank_list=read_csv(self.csv_saving_path_today)
+                        # sort_content_by_date(temp_tank_list)
+                        # save_csv(self.csv_saving_path_today,temp_tank_list)
+                        # del temp_tank_list
+                        # print('数据整理完毕')
                         '''重设数据'''
                         self.date_today_datetime+=datetime.timedelta(days=1)
                         self.date_today_string=self.date_today_datetime.strftime('%Y-%m-%d')
                         self.date_tomorrow_datetime=self.date_today_datetime+datetime.timedelta(days=1)
-                        self.date_tomorrow_string=self.date_tomorrow_datetime.strftime('&Y-%m-%d')
+                        self.date_tomorrow_string=self.date_tomorrow_datetime.strftime('%Y-%m-%d')
                         self.csv_saving_path_today=self.path+self.date_today_string+'/'+self.date_today_string+'.csv'
                         self.log_saving_path_today=self.path+self.date_today_string+'/'+self.date_today_string+'_log.txt'
 
@@ -278,37 +298,47 @@ class Async_infi_Spider():
                     elif self.content[-1][0][:10]==self.date_today_string:
 
                         save_csv(self.csv_saving_path_today,self.content)
+                        self.content=[]
                         print('当天数据储存完毕')
+                '''修改起始点'''
+                self.input_start_code=self.reached_max_code
                 '''数据储存完毕 查看是否需要修改 爬行速度'''
-                if datetime.datetime.now()-self.last_post_datetime>datetime.timedelta(hours=1):
+                if datetime.datetime.now()-self.last_post_datetime>datetime.timedelta(minutes=30):
                     self.concurrency=self.max_concurrency
-                    print('并发速度为最大限速 %s'%self.concurrency)
-                elif datetime.datetime.now()-self.last_post_datetime<=datetime.timedelta(hours=1):
+                    self.sleep_flag=0
+                    print('并发速度为最大限速 %s'%self.max_concurrency)
+                elif datetime.datetime.now()-self.last_post_datetime<=datetime.timedelta(minutes=30):
+                    self.sleep_flag=1
                     if self.post_vol==0:
                         self.concurrency=max(self.concurrency,self.concurrency_temp_step)
                         print('未获得发帖速度 %s'%self.concurrency)
                     else:
                         seconds=abs((datetime.datetime.now()-self.last_post_datetime).seconds)
-                        multi_para=0.003*seconds-0.8
-                        self.concurrency=max(int(multi_para*self.post_vol*60),self.concurrency_temp_step)
+                        multi_para=max(0,0.0127*seconds-2.8)
+                        self.concurrency=min(self.max_concurrency,max(int(multi_para*self.post_vol*60),self.concurrency_temp_step))
                         print('调整并发速度 %s'%self.concurrency)
+                self.return_count=0
+                self.concurrency_temp_step=10
             t2=time.time()
-            print('time cost %s'%(t2-t1))
-            if t2-t1<60:
+            print('time cost %.2f'%(t2-t1))
+            if t2-t1<60 and t2-t1>0 and self.sleep_flag==1:
                 print('sleeping...')
                 time.sleep(60-(t2-t1))
+            print('\n')
 
 
-def main(start_id,requests_amount,requests_per_time):
+def main(start_id,requests_amount,max_amount,requests_per_time):
     print('初始化')
     print('正在获取  起始id  起始date')
+    if not os.path.exists('./guba_crawled_data/'):
+        os.mkdir('./guba_crawled_data/')
     start_code,datetime_date=get_start_id_date('./guba_crawled_data/',start_id)
     print('获取完毕')
     print('起始id:%s 起始date:%s 起始并发数:%s'%(start_code,datetime_date,requests_amount))
     print('启动爬虫')
-    sever_spider=Async_infi_Spider(start_code,datetime_date,requests_amount,requests_per_time)
+    sever_spider=Async_infi_Spider(start_code,datetime_date,concurrency=requests_amount,max_concurrency=max_amount,num_per=requests_per_time)
     sever_spider._run()
 if __name__ == '__main__':
-    main(428106962,800,500)
+    main(428327774,1000,4000,500)
 
 
